3dc88bdb023f0721eb969458761bb51f961076de
traversa942 <traversa942@gmail.com>
2024-12-09 00:30:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -58,5 +58,4 @@ def detect():
 
 if __name__ == '__main__':
     # Run the app on the specified port
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
